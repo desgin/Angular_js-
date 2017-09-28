36c8b81bbb63a6ed7cc43ce1300b23431d70b515
Devi <devifrontenddev@gmail.com>
2017-09-28 18:01:24 -0400

    Add files via upload

---
+++ angular_js/filter_example/filter_example.js
@@ -0,0 +1,9 @@
+var app= angular.module("myApp", []);
+app.controller("myCtrl", function($scope)
+{
+$scope.firstname:'uday',
+$scope.lastname:'chiru',
+$scope.salary:'15000',
+$scope.date:'12/10/2015 03:28'
+
+});
